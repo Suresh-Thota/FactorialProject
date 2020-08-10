8fe6521751c0a60137937fc4e413527930252e65
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-10 21:30:18 +0530

    Add project files.

---
+++ FactorialProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FactorialProject
+{
+    class Program
+    {
+        
+        public static int Factorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            
+            
+                int n;
+                Console.WriteLine("enter a number");
+
+                n = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("factorial of " + n + " is " + Factorial(n));
+            
+
+        }
+    }
+}
